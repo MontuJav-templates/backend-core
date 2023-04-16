b8e9d4a0794645b103d3f6efd93a0f14e846dbe2
edoomm <edoomm8@gmail.com>
2023-04-16 11:38:01 +0200

    Implemented click handling

--- base/src/modules/FourButtons.js
+++ base/src/modules/FourButtons.js
@@ -2,14 +2,18 @@ import React from 'react';
 import Button from 'react-bootstrap/Button';
 
 function FourButtons() {
-  return (
-    <div>
-      <Button variant="primary">Upload LAS file</Button>{' '}
-      <Button variant="secondary">Process LAS file</Button>{' '}
-      <Button variant="success">Plot crossplot</Button>{' '}
-      <Button variant="danger">Cluster crossplot</Button>{' '}
-    </div>
-  );
+    const handleClick = () => {
+        console.log('Hello, world!');
+    };
+
+    return (
+        <div>
+        <Button variant="primary" onClick={handleClick}>Upload LAS file</Button>{' '}
+        <Button variant="secondary">Process LAS file</Button>{' '}
+        <Button variant="success">Plot crossplot</Button>{' '}
+        <Button variant="danger">Cluster crossplot</Button>{' '}
+        </div>
+    );
 }
 
 export default FourButtons;
