d91c26486d57600cdac6a99ca4f6310d14563ffa
edoomm <edoomm8@gmail.com>
2023-04-24 21:21:43 +0200

    Implemented showing crossplot, Too many warnings because of creating crossplot outside main thread. Still need to look for a good LAS file.

--- base/src/server/modules/las/las_handler.py
+++ base/src/server/modules/las/las_handler.py
@@ -1,12 +1,14 @@
 import lasio
+import matplotlib.pyplot as plt
 
 import werkzeug.datastructures
 
 class LAS_Handler:
-    def __init__(self, file: werkzeug.datastructures.FileStorage) -> None:
+    def __init__(self, file: werkzeug.datastructures.FileStorage, verbose=False) -> None:
         """Class responsible of handling LAS files.
         """
-        print(f"Trying to upload file of type {type(file)}...")
+        if verbose: print(f"Trying to upload file of type {type(file)}...")
+        self.filename = file.filename
         self.las_file = self.read_las_file(file)
     
     def read_las_file(self, file: werkzeug.datastructures.FileStorage) -> lasio.LASFile:
@@ -26,8 +28,23 @@ class LAS_Handler:
         if not self.las_file:
             print("No LAS file to work with")
             return
-        # Access the header information
-        header = self.las_file.header
+        
+        def get_header_info(): return self.las_file.header
+        def get_las_props(): return self.las_file.keys()
+        
+        # print(get_las_props())
+        self.show_crossplot('DT', 'RHOB')
+        print(f"Test of {self.filename} done successfully")
+    
+    def show_crossplot(self, x_property: str, y_property: str) -> None:
+        """Show crossplot of two properties from LAS file.
+        """
+        x = self.las_file[x_property]
+        y = self.las_file[y_property]
 
-        print(header)
-        print("Header read successfully")
+        plt.scatter(x, y)
+
+        plt.xlabel(x_property)
+        plt.ylabel(y_property)
+
+        plt.show()
