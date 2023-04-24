1b4f465d4444e75dbeeff5884ee293746649164d
edoomm <edoomm8@gmail.com>
2023-04-24 22:15:44 +0200

    Crossplots finished.

--- base/src/server/modules/las/las_handler.py
+++ base/src/server/modules/las/las_handler.py
@@ -1,3 +1,4 @@
+import sys
 import lasio
 import matplotlib.pyplot as plt
 
@@ -8,8 +9,10 @@ class LAS_Handler:
         """Class responsible of handling LAS files.
         """
         if verbose: print(f"Trying to upload file of type {type(file)}...")
+
         self.filename = file.filename
         self.las_file = self.read_las_file(file)
+        self.properties = self.las_file.keys()
     
     def read_las_file(self, file: werkzeug.datastructures.FileStorage) -> lasio.LASFile:
         """Read a LAS file.
@@ -32,10 +35,38 @@ class LAS_Handler:
         def get_header_info(): return self.las_file.header
         def get_las_props(): return self.las_file.keys()
         
-        # print(get_las_props())
-        self.show_crossplot('DT', 'RHOB')
-        print(f"Test of {self.filename} done successfully")
+        # print(self.choose_properties())
+        self.run_crossplot()
     
+    def run_crossplot(self):
+        """Run the main crossplot process
+        """
+        x, y = self.choose_properties()
+        self.show_crossplot(x, y)
+
+    def choose_properties(self) -> tuple[str,str]:
+        """Let user choose properties
+        """
+        # Showing menu
+        self.show_properties()
+        print("Choose your properties")
+        print("------------------------\n")
+        # TODO: Unhandled exceptions
+        op1 = input('Property 1: ')
+        op2 = input('Property 2: ')
+
+        return op1, op2
+
+    def show_properties(self) -> None:
+        """Show properties available in a LAS file.
+        """
+        props = "Properties available:\n"
+
+        for prop in self.properties:
+            props += f"{prop} - {self.las_file[prop]}\n"
+
+        print(props)
+
     def show_crossplot(self, x_property: str, y_property: str) -> None:
         """Show crossplot of two properties from LAS file.
         """
@@ -48,3 +79,6 @@ class LAS_Handler:
         plt.ylabel(y_property)
 
         plt.show()
+
+        print("Finishing program due to warning issues")
+        sys.exit(0)
