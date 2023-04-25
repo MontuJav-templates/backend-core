852c23ef04301853e2bdd1ecdd865ba231ad6d2a
Eduardo Mendoza <57675992+edoomm@users.noreply.github.com>
2023-04-25 20:34:23 +0200

    Crossplots (#2)
    * Updated gitignore
    Added react files ignoring
    
    * Intialized react project
    
    * Added react-bootstrap package
    
    * Added buttons
    
    * Implemented click handling
    
    * Implemented select button
    
    * Made simplier file selection
    
    * Connected back end and front end
    
    * Moved main server
    
    * Implemented reading of LAS files
    
    * Added .las filter
    
    * Updated TODO's README
    
    * Implemented showing crossplot,
    Too many warnings because of creating crossplot outside main thread.
    Still need to look for a good LAS file.
    
    * Crossplots finished.
    
    * First good example of a LAS file that can be crossplotted.
    
    * Updated README TODO

--- base/src/server/modules/las/las_handler.py
+++ base/src/server/modules/las/las_handler.py
@@ -1,13 +1,18 @@
+import sys
 import lasio
+import matplotlib.pyplot as plt
 
 import werkzeug.datastructures
 
 class LAS_Handler:
-    def __init__(self, file: werkzeug.datastructures.FileStorage) -> None:
+    def __init__(self, file: werkzeug.datastructures.FileStorage, verbose=False) -> None:
         """Class responsible of handling LAS files.
         """
-        print(f"Trying to upload file of type {type(file)}...")
+        if verbose: print(f"Trying to upload file of type {type(file)}...")
+
+        self.filename = file.filename
         self.las_file = self.read_las_file(file)
+        self.properties = self.las_file.keys()
     
     def read_las_file(self, file: werkzeug.datastructures.FileStorage) -> lasio.LASFile:
         """Read a LAS file.
@@ -26,8 +31,54 @@ class LAS_Handler:
         if not self.las_file:
             print("No LAS file to work with")
             return
-        # Access the header information
-        header = self.las_file.header
+        
+        def get_header_info(): return self.las_file.header
+        def get_las_props(): return self.las_file.keys()
+        
+        # print(self.choose_properties())
+        self.run_crossplot()
+    
+    def run_crossplot(self):
+        """Run the main crossplot process
+        """
+        x, y = self.choose_properties()
+        self.show_crossplot(x, y)
 
-        print(header)
-        print("Header read successfully")
+    def choose_properties(self) -> tuple[str,str]:
+        """Let user choose properties
+        """
+        # Showing menu
+        self.show_properties()
+        print("Choose your properties")
+        print("------------------------\n")
+        # TODO: Unhandled exceptions
+        op1 = input('Property 1: ')
+        op2 = input('Property 2: ')
+
+        return op1, op2
+
+    def show_properties(self) -> None:
+        """Show properties available in a LAS file.
+        """
+        props = "Properties available:\n"
+
+        for prop in self.properties:
+            props += f"{prop} - {self.las_file[prop]}\n"
+
+        print(props)
+
+    def show_crossplot(self, x_property: str, y_property: str) -> None:
+        """Show crossplot of two properties from LAS file.
+        """
+        x = self.las_file[x_property]
+        y = self.las_file[y_property]
+
+        plt.scatter(x, y)
+
+        plt.xlabel(x_property)
+        plt.ylabel(y_property)
+
+        plt.show()
+
+        print("Finishing program due to warning issues")
+        sys.exit(0)
